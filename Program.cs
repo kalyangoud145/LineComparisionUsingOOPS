6203f76ab96ad9f6030c991d7b912bb2a439a0ea
kalyangoud145 <kalyangoud145@gmail.com>
2020-10-25 17:28:22 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace LineComparisionUsingOOPS
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome To Line Comparision Problem");
+        }
+    }
+}
