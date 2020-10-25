7f92d7458198d91fb28b8cb983beeef271974afc
kalyangoud145 <kalyangoud145@gmail.com>
2020-10-25 18:02:31 +0530

    [Kalyan] Finding equality of two lines

--- Program.cs
+++ Program.cs
@@ -15,6 +15,19 @@ namespace LineComparisionUsingOOPS
             double temp2 = lineComparisionImpl.GetEndPointsOfY();
             double result1 = lineComparisionImpl.DistanceBtwnPoints(temp1, temp2);
             Console.WriteLine("Distance between two points: " + result1);
+            double temp3 = lineComparisionImpl.GetEndPointsOfX();
+            double temp4 = lineComparisionImpl.GetEndPointsOfY();
+            double result2 = lineComparisionImpl.DistanceBtwnPoints(temp3, temp4);
+            Console.WriteLine("Distance between two points: " + result2);
+            bool checkLines = result1.Equals(result2);
+            if (checkLines == true)
+            {
+                Console.WriteLine("Both lines are equal");
+            }
+            else
+            {
+                Console.WriteLine("lines are not equal");
+            }
         }
     }
 }
